bf13925b079518abe5863cf05a87fedc59108c49
adofsauron <adofsauron@163.com>
2022-11-30 20:00:07 +0800

    fix innodb block

--- run_analyze.sh
+++ run_analyze.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo ./innblock  ./data/t1.ibd  3 16
 ./innblock  ./data/t1.ibd  3 16
--- run_scan.sh
+++ run_scan.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo ./innblock  ./data/t1.ibd  scan 16
 ./innblock  ./data/t1.ibd  scan 16
